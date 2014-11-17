d887c3bcf3103ef2ce267820ef84d16d027b5d01
Joseph Tsai <victoryandjoseph@gmail.com>
2014-11-17 22:11:42 +0800

    move to vc folder

---
+++ viewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cusViewController
+//
+//  Created by Joseph on 2014/11/17.
+//  Copyright (c) 2014年 dosomethingq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
